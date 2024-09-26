7d772d81febd7de5df0b04c09ab133e45f307a1a
2ag3.iorilope <2ag3.iorilope@ENARA-P22>
2024-09-26 09:50:38 +0200

    Agregar archivos de proyecto.

---
+++ Ordezkaria/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Ordezkaria
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Ordezkaria/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.ObjectModel;
+using Microsoft.Maui.Controls;
+
+namespace Ordezkaria
+{
+    public partial class MainPage : ContentPage
+    {
+        private ObservableCollection<string> names;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            names = new ObservableCollection<string>();
+           IzenakListView.ItemsSource = names; // Asignar la fuente de datos al ListView
+        }
+
+        private void OnAddButtonClicked(object sender, EventArgs e)
+        {
+            string name = izenaEntry.Text;
+
+            if (!string.IsNullOrWhiteSpace(name))
+            {
+                names.Add(name); // Añadir el nombre a la colección
+                izenaEntry.Text = string.Empty; // Limpiar el Entry
+            }
+        }
+    }
+}
